cf518da031e65e064c09094c5f82e8072daa6066
Виталий <dom200989@gmail.com>
2021-02-26 15:18:05 +0300

    AboutView предварительная версия: готово

--- src/main/java/com/powerLife/myTask/view/AboutView.java
+++ src/main/java/com/powerLife/myTask/view/AboutView.java
@@ -1,42 +1,39 @@
 package com.powerLife.myTask.view;
 
-
 import javax.swing.*;
 import java.awt.*;
 
 public class AboutView extends JDialog {
 
-    private final JPanel jPanel = new JPanel();
-    private final JLabel jLabel1 = new JLabel();
-    private final JLabel jLabel2 = new JLabel();
-    private final JButton jButton = new JButton("ок");
-    private final String text = "Задача о ходе коня";
-    private final String test = "<html><h2>What is Google Labs?</h2>" +
-            "<font face=’verdana’ size = 2>" +
-            " Google Labs is a playground <br>" +
-            " where our more adventurous users can play around with <br>" +
-            " prototypes of some of our wild and crazy ideas and <br>" +
-            " offer feedback directly to the engineers who developed<br>" +
-            " them. Please note that Labs is the first phase in <br>" +
-            " a lengthy product development process and none of this <br>" +
-            " stuff is guaranteed to make it onto Google.com. <br>" +
-            " While some of our crazy ideas might grow into the <br>" +
-            " next Gmail or iGoogle, others might turn out to be, <br>" +
-            " well, just plain crazy.</html>";
-
     public AboutView() { // TODO: 24.02.2021 Реализовать содержимое окна
-        jLabel1.setText(text);
-        jLabel1.setHorizontalAlignment(JLabel.CENTER);
-        jLabel1.setForeground(Color.DARK_GRAY);
-        jLabel1.setFont(new Font(null, Font.BOLD, 30));
-        jPanel.add(jLabel1);
+        JPanel jPanel = new JPanel();
 
-        jLabel2.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
-        jLabel2.setText(test);
-        jLabel2.setHorizontalAlignment(JLabel.CENTER);
-        jLabel2.setForeground(Color.BLUE);
-        jLabel2.setFont(new Font(null, Font.BOLD, 20));
-        jPanel.add(jLabel2);
+        JLabel head = new JLabel("A knight's tour");
+        head.setHorizontalAlignment(JLabel.CENTER);
+        head.setForeground(Color.DARK_GRAY);
+        head.setFont(new Font("Georgia", Font.BOLD, 30));
+        jPanel.add(head);
+
+        JLabel textEn = new JLabel("<html><p align=\"center\">" +
+                "A knight's tour is a sequence of moves of a knight<</br>\n" +
+                "<br>on a chessboard such that the knight visits every</br>\n" +
+                "<br>square exactly once. If the knight ends on a square</br>\n" +
+                "<br>that is one knight's move from the beginning square</br>\n" +
+                "<br>(so that it could tour the board again immediately,</br>\n" +
+                "<br>following the same path), the tour is closed;</br>\n" +
+                "<br>otherwise, it is open.</br>\n" +
+                "<br>The knight's tour problem is the mathematical problem</br>\n" +
+                "<br>of finding a knight's tour. Creating a program to</br>\n" +
+                "<br>find a knight's tour is a common problem given to</br>\n" +
+                "<br>computer science students. Variations of the knight's</br>\n" +
+                "<br>tour problem involve chessboards of different sizes</br>\n" +
+                "<br>than the usual 8 × 8, as well as irregular</br>\n" +
+                "<br>(non-rectangular) boards.</br>\n" +
+                "</p></html>");
+        textEn.setHorizontalAlignment(JLabel.CENTER);
+        textEn.setForeground(Color.BLACK);
+        textEn.setFont(new Font("Georgia", Font.BOLD, 13));
+        jPanel.add(textEn);
 
         add(jPanel);
         setModal(true);
